28945132cbe6f80c7a49d11f3f9087ee6c83ba7d
Pey Lian Lim <lim@stsci.edu>
2017-01-31 22:13:40 -0500

    Added CaptainBarnacle

--- creature_report/report.py
+++ creature_report/report.py
@@ -1,8 +1,14 @@
 """Module to handle Pandokia targeted reporting."""
 from __future__ import absolute_import, division, print_function
 
+import glob
 import os
+import stat
+import time
 from collections import Counter
+from datetime import datetime
+
+__all__ = ['CreatureReport', 'CaptainBarnacle', 'rm_old_reps']
 
 
 class CreatureReport(object):
@@ -96,3 +102,81 @@ class CreatureReport(object):
                         fout.write('</p>\n')
 
             fout.write('</body>\n</html>\n')
+
+
+class CaptainBarnacle(object):
+    """Class to automate :class:`CreatureReport`."""
+    def __init__(self, date_str=datetime.today().strftime('%Y%m%d')):
+        self.path = os.environ['REMOTE_DIR']
+        self.outpath = os.environ['HTML_DIR']
+        self.pdklog = os.path.join(self.path, 'pdklog{}.txt'.format(date_str))
+        self.html = os.path.join(self.outpath, 'rep{}.html'.format(date_str))
+
+    def daily_report(self):
+        """Generate daily report."""
+        c = CreatureReport()
+        c.parse_log(self.pdklog)
+        c.to_html(self.html, overwrite=True)
+
+        # Add group write permission.
+        os.chmod(self.html, os.stat(self.html).st_mode | stat.S_IWGRP)
+
+    def symlink_results(self):
+        """Create symbolic link in ``REMOTE_DIR`` to the latest report."""
+        dstfile = os.path.join(self.outpath, 'daily_report.html')
+        if os.path.lexists(dstfile):
+            os.unlink(dstfile)
+        os.symlink(self.html, dstfile)
+
+
+def rm_old_reps(root, pattern='rep*.html', max_life=7.0, verbose=True):
+    """Delete all files matching ``pattern`` in ``root``
+    older than ``max_life`` days.
+
+    Parameters
+    ----------
+    root : string
+        Root directory containing files to delete.
+
+    pattern : string
+        File ``glob`` pattern.
+
+    max_life : float
+        Maximum allowed life span in days. Naively
+        assumes there are 86400 seconds in a day.
+
+    verbose : bool
+        Extra log info.
+
+    """
+    max_sec = 86400.0 * max_life
+    t_now = time.time()  # sec
+
+    for f in glob.iglob(os.path.join(root, pattern)):
+        if not os.path.isfile(f):
+            continue
+
+        if (t_now - os.stat(f).st_mtime) < max_sec:
+            if verbose:
+                print('{} is newer than {} day(s) '
+                      '-- skipped'.format(f, max_life))
+            continue
+
+        try:
+            os.remove(f)
+        except Exception as e:
+            if verbose:
+                print('{} cannot be deleted -- {}'.format(f, str(e)))
+        else:
+            if verbose:
+                print('{} successfully deleted'.format(f))
+
+
+if __name__ == '__main__':
+    """Batch script for cron job."""
+    r = CaptainBarnacle()
+    r.daily_report()
+    r.symlink_results()
+
+    rm_old_reps(os.environ['REMOTE_DIR'], pattern='pdklog*.txt')
+    rm_old_reps(os.environ['HTML_DIR'])
